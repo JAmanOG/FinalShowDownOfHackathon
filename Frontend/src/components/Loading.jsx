19d7fa215b89ebd18e34f5174a3a516d9e355162
Bhavik Vimal Prajapati <bhavikp0026@gmail.com>
2025-01-19 07:00:13 +0530

    -

--- Frontend/src/components/Loading.jsx
+++ Frontend/src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = ({ size = 'w-10 h-10', color = 'text-blue-500', message = 'Loading...' }) => {
   return (
-    <div className="flex flex-col items-center justify-center space-y-2">
+    <div className="flex flex-col items-center justify-center space-y-2 h-[750px]">
       <div
         className={`animate-spin rounded-full border-t-4 border-l-4 border-gray-200 ${color} ${size}`}
       ></div>
